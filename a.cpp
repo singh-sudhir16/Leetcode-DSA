f28ce22bba81625157b3b36047b69e9e53979932
Sudhir Singh <mail.sudhir.singh14@gmail.com>
2025-05-14 20:20:48 +0530

    add a.cpp

---
+++ a.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define pb push_back
+#define all(v) v.begin(), v.end()
+
+int main() {
+    fast_io;
+
+    // Your code here
+
+    return 0;
+}

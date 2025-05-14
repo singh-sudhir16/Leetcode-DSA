d530e6badfa8f646cde60a480f4ec810fb69e986
Sudhir Singh <mail.sudhir.singh14@gmail.com>
2025-05-14 20:23:50 +0530

    add a.cpp

--- a.cpp
+++ a.cpp
@@ -25,5 +25,14 @@ int main() {
     unordered_map<int, int> mp; // to store the answers for each element
     stack<int> st;
     int n = nums2.size();
+    for (int i = n - 1; i >= 0; i--) {
+        while (!st.empty() && st.top() <= nums2[i]) {
+            st.pop();
+        }
+        int res = st.empty() ? -1 : st.top();
+        st.push(nums2[i]);
+        mp[nums2[i]] = res;
+    }
+    vector<int> res;
     return 0;
 }
